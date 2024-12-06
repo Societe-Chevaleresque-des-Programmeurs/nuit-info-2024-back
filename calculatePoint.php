79913b98e070a1d3b78b7946240dd906e4c3e8fe
romain327 <romain@brouard.dev>
2024-12-06 01:20:33 +0100

    modifs calculatePoint

--- calculatePoint.php
+++ calculatePoint.php
@@ -1,26 +1,45 @@
 <?php
+error_reporting(E_ALL);
+require_once("db_connection.php");
+$html = file_get_contents("index.php");
+
 $coefRight = 1;
 $coefWrong = 1;
 $nbRightAnswer = 0;
 $nbWrongAnswer = 0;
 
-$procedure_params = array(
-array(&$myparams['login'], SQLSRV_PARAM_IN));
-$sql = "EXEC getRightAnswers login = ?";
+$login = $_SESSION['user'];
+
+$sql = "CALL getRightAnswers(?)";
 $stmt = mysqli_prepare($con, $sql);
+if ($stmt === false) {
+    die('Error preparing statement: ' . mysqli_error($con));
+}
+mysqli_stmt_bind_param($stmt, 's', $login);
 mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
-if ($result){
+if ($result) {
     $user_data = mysqli_fetch_assoc($result);
-    $nbRightAnswer = $user_data['value']; // to correct
+    $nbRightAnswer = $user_data['sumR']; // Assuming the column name is 'value'
 }
-$sql = "EXEC getWrongAnswers login = ?";
-$stmt = mysqli_prepare($conn, $sql);
+mysqli_stmt_close($stmt);
+
+// Get wrong answers
+$sql = "CALL getWrongAnswers(?)";
+$stmt = mysqli_prepare($con, $sql);
+if ($stmt === false) {
+    die('Error preparing statement: ' . mysqli_error($con));
+}
+mysqli_stmt_bind_param($stmt, 's', $login);
 mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
-if ($result){
+if ($result) {
     $user_data = mysqli_fetch_assoc($result);
-    $nbWrongAnswer = $user_data['value']; // to correct
+    $nbWrongAnswer = $user_data['sumR']; // Assuming the column name is 'value'
 }
+mysqli_stmt_close($stmt);
 
 $nbScore = $coefRight * $nbRightAnswer + $coefWrong * $nbWrongAnswer;
+$html = str_replace("{{ score }}", $nbScore, $html);
+
+echo $html;
